37cc80a0aead4cde3191fba75ec19c30e1b241d9
duanhongyi <duanhongyi@doopai.com>
2016-01-14 16:04:46 +0800

    "use tmk soft uart"

--- nrf51822.mk
+++ nrf51822.mk
@@ -1,8 +1,9 @@
 NRF51822_DIR = src/nrf51822
 
+SRC += protocol/serial_soft.c
+
 SRC += src/nrf51822/main.c \
-	src/nrf51822/nrf51822_driver.c \
-	src/nrf51822/serial_soft.c
+	src/nrf51822/nrf51822_driver.c
 
 
 OPT_DEFS += -DPROTOCOL_NRF51822
